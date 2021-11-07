08c99ec6c0b558d3cdba613c0f276ad18e44bc53
Pavel Uvarov <44322010+p-uvarov@users.noreply.github.com>
2021-11-07 18:30:17 -0500

    (PR #6) Added Green dog bowl checkmark to the History Screen and the Product Screen

--- Chewhuahua/Views/History Screen/HistoryView.swift
+++ Chewhuahua/Views/History Screen/HistoryView.swift
@@ -15,65 +15,74 @@ struct HistoryView: View {
 	
 	var body: some View {
 		NavigationView {
-			if viewModel.productViewRecords.isEmpty {
-				Text("You haven't scanned anything yet")
-					.font(.system(size: 20, weight: .bold, design: .rounded))
-					.padding(.horizontal, 20)
-			} else {
-				ScrollView {
-					VStack {
-						ForEach(viewModel.productViewRecords, id: \.id) { record in
-							NavigationLink {
-								makeProductView(record.product.barcode, false)
-									.navigationTitle("Product Details")
-									.navigationBarTitleDisplayMode(.inline)
-							} label: {
-							HStack {
-								VStack {
-									AsyncImage(url: URL(string: record.product.photoURL), content: { image in
-										image
-											.resizable()
-											.scaledToFit()
-									}) {
-										ProgressView()
+			VStack {
+				if viewModel.productViewRecords.isEmpty {
+					Text("You haven't scanned anything yet")
+						.font(.system(size: 20, weight: .bold, design: .rounded))
+						.padding(.horizontal, 20)
+				} else {
+					ScrollView {
+						VStack {
+							ForEach(viewModel.productViewRecords, id: \.id) { record in
+								NavigationLink {
+									makeProductView(record.product.barcode, false)
+										.navigationTitle("Product Details")
+										.navigationBarTitleDisplayMode(.inline)
+								} label: {
+									HStack {
+										VStack {
+											AsyncImage(url: URL(string: record.product.photoURL), content: { image in
+												image
+													.resizable()
+													.scaledToFit()
+											}) {
+												ProgressView()
+											}
+											.frame(width: 150, height: 150)
+											.padding(5)
+										}
+										Spacer()
+										VStack(alignment: .leading, spacing: 5) {
+											Group {
+												Text(record.product.brand)
+													.font(.system(size: 16, weight: .semibold, design: .rounded))
+													.foregroundColor(.brandOrange)
+												
+												Text(record.product.name)
+													.font(.system(size: 18, weight: .semibold, design: .rounded))
+													.fontWeight(.bold)
+													.foregroundColor(.black)
+													.multilineTextAlignment(.leading)
+												
+												Text("\(record.product.weight) kg")
+													.font(.system(size: 16, weight: .semibold, design: .rounded))
+													.foregroundColor(.black)
+												
+												Text("$\(record.product.price)")
+													.font(.system(size: 22, weight: .bold, design: .rounded))
+													.foregroundColor(.black)
+											}
+										}
+										.overlay(Image("green_dog_bowl_check")
+													.resizable()
+													.scaledToFit()
+													.frame(height: 40)
+												 , alignment: .bottomTrailing)
 									}
-									.frame(width: 150, height: 150)
-									.padding(5)
+									.padding(15)
+									.background(.white)
+									.cornerRadius(15)
+									.shadow(color: .gray, radius: 5)
+									.padding(.vertical, 5)
 								}
-								Spacer()
-								VStack(alignment: .leading, spacing: 5) {
-									Group {
-										Text(record.product.brand)
-											.font(.system(size: 16, weight: .semibold, design: .rounded))
-											.foregroundColor(.brandOrange)
-										
-										Text(record.product.name)
-											.font(.system(size: 18, weight: .semibold, design: .rounded))
-											.fontWeight(.bold)
-											.foregroundColor(.black)
-											.multilineTextAlignment(.leading)
-										
-										Text("\(record.product.weight) kg")
-											.font(.system(size: 16, weight: .semibold, design: .rounded))
-											.foregroundColor(.black)
-										
-										Text("$\(record.product.price)")
-											.font(.system(size: 22, weight: .bold, design: .rounded))
-											.foregroundColor(.black)
-									}
-								}
-							}
-							.padding(10)
-							.background(.white)
-							.cornerRadius(15)
-							.shadow(color: .gray, radius: 5)
-							.padding(.vertical, 5)
 							}
 						}
+						.padding(.horizontal, 20)
 					}
-					.padding(.horizontal, 20)
 				}
 			}
+			.navigationTitle("History")
+			.navigationBarTitleDisplayMode(.inline)
 		}
 		.onAppear(perform: onAppear)
 	}
